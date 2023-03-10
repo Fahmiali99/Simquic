1d0b3e4b5486d573c8cce261391ee85127da4987
Fahmi <fahmiali130499@gmail.com>
2023-03-10 13:39:57 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./Components/Footer";
+import LandingPage from "./Pages/LandingPage";
+import InboxPage from "./Pages/InboxPage";
+import TaskPage from "./Pages/TaskPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={LandingPage} />
+        <Route path="/inbox" element={InboxPage} />
+        <Route path="/task" element={TaskPage} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
